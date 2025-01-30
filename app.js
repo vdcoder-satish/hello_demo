cab0a9d83b8f6f4f8daa833e1f7d0c632e5d3e41
satish <satishnengineer@gmail.com>
2025-01-30 08:54:51 +0530

    adding end point

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const app=express();
 app.listen(3000,()=>{
   console.log('server running on port 3000')
 })
+
+app.get('/get-list',async(req,res)=>{
+  const data={
+    name:"abc",
+    email:"abc@gmailc.com",
+    mobil:8989898989
+  }
+  res.status(200).json({message:"successfully",data})
+})
